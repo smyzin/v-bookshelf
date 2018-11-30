cadb438221d9c45cb7fe355375da4634f8985c3c
Sergei <smyzin.work@gmail.com>
2018-11-30 17:42:36 +0300

    Rebuild commit

--- app/config/index.js
+++ app/config/index.js
@@ -6,8 +6,8 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../../server/public/index.html'),
-    assetsRoot: path.resolve(__dirname, '../../server/public'),
+    index: path.resolve(__dirname, '../../public/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
